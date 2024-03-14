26474af0cd6b7a33303211cf9605806bb11f7a36
IskanderSh <is2367361@gmail.com>
2024-03-14 10:09:07 +0000

    add generated files

---
+++ internal/handler/create_actor.go
@@ -0,0 +1,5 @@
+package handlers
+
+func (h *Handler) CreateActor(w http.ResponseWriter, r *http.Request) {
+	return middleware.NotImplemented("operation actor CreateActor has not yet been implemented")
+}
